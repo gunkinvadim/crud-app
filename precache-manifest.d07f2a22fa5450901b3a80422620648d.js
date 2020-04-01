186094efee6e6c676545981b4556b4422ea4d5f1
gunkin.vadim <gunkin.vadim@ya.ru>
2020-04-01 21:23:11 +0300

    Updates

---
+++ precache-manifest.d07f2a22fa5450901b3a80422620648d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d514edd1df5c721f8b6cac065e21ce1a",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "e3c0fc8ecdb113502d0a",
+    "url": "/crud-app/static/css/main.70070990.chunk.css"
+  },
+  {
+    "revision": "98931f55001229f1ff4b",
+    "url": "/crud-app/static/js/2.bceb1b15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crud-app/static/js/2.bceb1b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c0fc8ecdb113502d0a",
+    "url": "/crud-app/static/js/main.6647148e.chunk.js"
+  },
+  {
+    "revision": "8a4824fbd7fb4becc715",
+    "url": "/crud-app/static/js/runtime-main.0e34417a.js"
+  }
+]);
